93445a9b84bca03c1693c320ece816e47efc5f69
Solar1s <solar1s@163.com>
2022-08-14 15:36:43 +0800

    Building a stub resolver

--- dns_in_rust/README.md
+++ dns_in_rust/README.md
@@ -14,3 +14,32 @@ czy@ubuntu:~/Desktop
 00000000  d2 4c 01 20 00 01 00 00  00 00 00 00 06 67 6f 6f  |.L. .........goo|
 00000010  67 6c 65 03 63 6f 6d 00  00 01 00 01              |gle.com.....|
 0000001c
+
+```shell
+DnsHeader {
+    id: 6666,
+    recursion_desired: true,
+    truncated_message: false,
+    authoritative_answer: false,
+    opcode: 0,
+    response: true,
+    rescode: NOERROR,
+    checking_disabled: false,
+    authed_data: false,
+    z: false,
+    recursion_available: true,
+    questions: 1,
+    answers: 1,
+    authoritative_entries: 0,
+    resource_entries: 0
+}
+DnsQuestion {
+    name: "google.com",
+    qtype: A
+}
+A {
+    domain: "google.com",
+    addr: 216.58.209.110,
+    ttl: 79
+}
+```
--- dns_in_rust/src/main.rs
+++ dns_in_rust/src/main.rs
@@ -1,7 +1,8 @@
 use std::error::Error;
-use std::fs::File;
-use std::io::Read;
+// use std::fs::File;
+// use std::io::Read;
 use std::net::Ipv4Addr;
+use std::net::UdpSocket;
 
 pub struct BytePacketBuffer {
     pub buf: [u8; 512],
@@ -165,6 +166,50 @@ impl BytePacketBuffer {
 
         Ok(())
     }
+
+    fn write(&mut self, val: u8) {
+        if self.pos >= 512 {
+            panic!("End of buffer");
+        }
+        self.buf[self.pos] = val;
+        self.pos += 1;
+    }
+
+    fn write_u8(&mut self, val: u8) {
+        self.write(val);
+    }
+
+    fn write_u16(&mut self, val: u16) {
+        self.write((val >> 8) as u8);
+        self.write((val & 0xFF) as u8);
+    }
+
+    fn write_u32(&mut self, val: u32) {
+        self.write(((val >> 24) & 0xFF) as u8);
+        self.write(((val >> 16) & 0xFF) as u8);
+        self.write(((val >> 8) & 0xFF) as u8);
+        self.write(((val >> 0) & 0xFF) as u8);
+    }
+
+    // We'll also need a function for writing query names in labeled form
+    fn write_qname(&mut self, qname: &str) -> Result<(), Box<dyn Error>> {
+        for label in qname.split('.') {
+            let len = label.len();
+            if len > 0x3f {
+                return Err("Single label exceeds 63 characters of length".into());
+            }
+
+            self.write_u8(len as u8);
+            for b in label.as_bytes() {
+                self.write_u8(*b);
+            }
+        }
+
+        self.write_u8(0);
+
+        Ok(())
+    }
+
 }
 
 
@@ -264,6 +309,33 @@ impl DnsHeader {
         // Return the constant header size
         Ok(())
     }
+
+    pub fn write(&self, buffer: &mut BytePacketBuffer) -> Result<(), Box<dyn Error>> {
+        buffer.write_u16(self.id);
+
+        buffer.write_u8(
+            (self.recursion_desired as u8)
+                | ((self.truncated_message as u8) << 1)
+                | ((self.authoritative_answer as u8) << 2)
+                | (self.opcode << 3)
+                | ((self.response as u8) << 7) as u8,
+        );
+
+        buffer.write_u8(
+            (self.rescode as u8)
+                | ((self.checking_disabled as u8) << 4)
+                | ((self.authed_data as u8) << 5)
+                | ((self.z as u8) << 6)
+                | ((self.recursion_available as u8) << 7),
+        );
+
+        buffer.write_u16(self.questions);
+        buffer.write_u16(self.answers);
+        buffer.write_u16(self.authoritative_entries);
+        buffer.write_u16(self.resource_entries);
+
+        Ok(())
+    }
 }
 
 
@@ -308,6 +380,16 @@ impl DnsQuestion {
         buffer.read_qname(&mut self.name);
         self.qtype = QueryType::from_num(buffer.read_u16()); // qtype
         let _ = buffer.read_u16(); // class
+
+        Ok(())
+    }
+
+    pub fn write(&self, buffer: &mut BytePacketBuffer) -> Result<(), Box<dyn Error>> {
+        buffer.write_qname(&self.name);
+
+        let typenum = self.qtype.to_num();
+        buffer.write_u16(typenum);
+        buffer.write_u16(1);
 
         Ok(())
     }
@@ -369,6 +451,36 @@ impl DnsRecord {
             }
         }
     }
+
+    
+    pub fn write(&self, buffer: &mut BytePacketBuffer) -> Result<usize, Box<dyn Error>> {
+        let start_pos = buffer.pos();
+
+        match *self {
+            DnsRecord::A {
+                ref domain,
+                ref addr,
+                ttl,
+            } => {
+                buffer.write_qname(domain)?;
+                buffer.write_u16(QueryType::A.to_num());
+                buffer.write_u16(1);
+                buffer.write_u32(ttl);
+                buffer.write_u16(4);
+
+                let octets = addr.octets();
+                buffer.write_u8(octets[0]);
+                buffer.write_u8(octets[1]);
+                buffer.write_u8(octets[2]);
+                buffer.write_u8(octets[3]);
+            }
+            DnsRecord::UNKNOWN { .. } => {
+                println!("Skipping record: {:?}", self);
+            }
+        }
+
+        Ok(buffer.pos() - start_pos)
+    }
 }
 
 #[derive(Clone, Debug)]
@@ -416,27 +528,84 @@ impl DnsPacket {
 
         Ok(result)
     }
+
+    
+    pub fn write(&mut self, buffer: &mut BytePacketBuffer) -> Result<(), Box<dyn Error>> {
+        self.header.questions = self.questions.len() as u16;
+        self.header.answers = self.answers.len() as u16;
+        self.header.authoritative_entries = self.authorities.len() as u16;
+        self.header.resource_entries = self.resources.len() as u16;
+
+        self.header.write(buffer)?;
+
+        for question in &self.questions {
+            question.write(buffer)?;
+        }
+        for rec in &self.answers {
+            rec.write(buffer)?;
+        }
+        for rec in &self.authorities {
+            rec.write(buffer)?;
+        }
+        for rec in &self.resources {
+            rec.write(buffer)?;
+        }
+
+        Ok(())
+    }
+
 }
 
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut f = File::open("response_packet.txt")?;
-    let mut buffer = BytePacketBuffer::new();
-    f.read(&mut buffer.buf)?;
+    // Perform an A query for google.com
+    let qname = "google.com";
+    let qtype = QueryType::A;
 
-    let packet = DnsPacket::from_buffer(&mut buffer)?;
-    println!("{:#?}", packet.header);
+    // Using googles public DNS server
+    let server = ("8.8.8.8", 53);
 
-    for q in packet.questions {
+    // Bind a UDP socket to an arbitrary port
+    let socket = UdpSocket::bind(("0.0.0.0", 43210))?;
+
+    // Build our query packet. It's important that we remember to set the
+    // `recursion_desired` flag. As noted earlier, the packet id is arbitrary.
+    let mut packet = DnsPacket::new();
+
+    packet.header.id = 6666;
+    packet.header.questions = 1;
+    packet.header.recursion_desired = true;
+    packet
+        .questions
+        .push(DnsQuestion::new(qname.to_string(), qtype));
+
+    // Use our new write method to write the packet to a buffer...
+    let mut req_buffer = BytePacketBuffer::new();
+    packet.write(&mut req_buffer)?;
+
+    // ...and send it off to the server using our socket:
+    socket.send_to(&req_buffer.buf[0..req_buffer.pos], server)?;
+
+    // To prepare for receiving the response, we'll create a new `BytePacketBuffer`,
+    // and ask the socket to write the response directly into our buffer.
+    let mut res_buffer = BytePacketBuffer::new();
+    socket.recv_from(&mut res_buffer.buf)?;
+
+    // As per the previous section, `DnsPacket::from_buffer()` is then used to
+    // actually parse the packet after which we can print the response.
+    let res_packet = DnsPacket::from_buffer(&mut res_buffer)?;
+    println!("{:#?}", res_packet.header);
+
+    for q in res_packet.questions {
         println!("{:#?}", q);
     }
-    for rec in packet.answers {
+    for rec in res_packet.answers {
         println!("{:#?}", rec);
     }
-    for rec in packet.authorities {
+    for rec in res_packet.authorities {
         println!("{:#?}", rec);
     }
-    for rec in packet.resources {
+    for rec in res_packet.resources {
         println!("{:#?}", rec);
     }
 
